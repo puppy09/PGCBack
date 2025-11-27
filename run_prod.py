f8709696886f0e2dbb1f8ffc03f00e7f67443c6e
Luis Mario <43504168+LuisM711@users.noreply.github.com>
2025-11-27 13:01:24 -0700

    asd

---
+++ run_prod.py
@@ -0,0 +1,14 @@
+from app import create_app, db
+from sqlalchemy import inspect
+
+
+app = create_app()
+
+with app.app_context():
+    inspector = inspect(db.engine)
+    tables = inspector.get_table_names()
+    print("Tablas",tables)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=False)
+
