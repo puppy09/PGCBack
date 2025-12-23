a566f7ad6a040049bdebe6c3222e69ae5e254ecf
Luis Mario <43504168+LuisM711@users.noreply.github.com>
2025-12-22 23:38:47 -0700

    cors

--- run_prod.py
+++ run_prod.py
@@ -15,8 +15,8 @@ with app.app_context():
 if __name__ == '__main__':
     # Configuración SSL con certificados de Let's Encrypt
     ssl_context = (
-        "/home/ubuntu/certs/fullchain.pem",
-        "/home/ubuntu/certs/privkey.pem"
+        "/etc/letsencrypt/live/proyectomedico.xyz/fullchain.pem",
+        "/etc/letsencrypt/live/proyectomedico.xyz/privkey.pem"
     )
     app.run(host='0.0.0.0', port=5000, debug=False, ssl_context=ssl_context)
 
